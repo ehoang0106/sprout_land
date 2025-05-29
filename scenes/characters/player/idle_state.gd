2bf20fbd359bee79b6996f4754e3651b8d493429
ehoang0106 <khoahoang1698@gmail.com>
2025-05-29 13:05:12 -0700

    done state machine

--- scenes/characters/player/idle_state.gd
+++ scenes/characters/player/idle_state.gd
@@ -1,40 +1,32 @@
 extends NodeState
 
-@export var player: CharacterBody2D
+@export var player: Player
 @export var animated_srpite_2d: AnimatedSprite2D
 
-var direction: Vector2
 
 func _on_process(_delta : float) -> void:
 	pass
 
 
 func _on_physics_process(_delta : float) -> void:
-	if Input.is_action_pressed("walk_left"):
-		direction = Vector2.LEFT
-	elif Input.is_action_pressed("walk_right"):
-		direction = Vector2.RIGHT
-	elif Input.is_action_pressed("walk_up"):
-		direction = Vector2.UP
-	elif Input.is_action_pressed("walk_down"):
-		direction = Vector2.DOWN
-	else:
-		direction = Vector2.ZERO
 		
-	if direction == Vector2.UP:
+	if player.play_direction == Vector2.UP:
 		animated_srpite_2d.play("idle_back")
-	elif direction == Vector2.RIGHT:
+	elif player.play_direction == Vector2.RIGHT:
 		animated_srpite_2d.play("idle_right")	
-	elif direction == Vector2.LEFT:
+	elif player.play_direction == Vector2.LEFT:
 		animated_srpite_2d.play("idle_left")
-	elif direction == Vector2.DOWN:
+	elif player.play_direction == Vector2.DOWN:
 		animated_srpite_2d.play("idle_front")
 	else:
 		animated_srpite_2d.play("idle_front")			
-	pass
+	
 
 
 func _on_next_transitions() -> void:
+	GameInputEvents.movement_input()
+	if GameInputEvents.is_movement_input():
+		transition.emit("Walk")
 	pass
 
 
@@ -43,4 +35,5 @@ func _on_enter() -> void:
 
 
 func _on_exit() -> void:
+	animated_srpite_2d.stop()
 	pass
---
+++ scenes/characters/player/player.gd
@@ -0,0 +1,3 @@
+class_name Player extends CharacterBody2D
+
+var play_direction: Vector2
